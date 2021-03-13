3f4cb306f05e8a790ed81993d276e8e6e197a830
Antonio Gonzalez <goantonio31@outlook.com>
2021-03-12 16:58:41 -0800

    updated branches and removed the main branch

--- db/joinTables.js
+++ db/joinTables.js
@@ -2,9 +2,6 @@ const { createRole, createEmployee, createDepartment } = require("./index");
 const App = require("../app.js");
 
 createRole();
-
 createEmployee();
-
 createDepartment();
-
 export default joinTable;
